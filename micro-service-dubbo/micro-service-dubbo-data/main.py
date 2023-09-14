1956818bc3326a2f272e2994dd30a8693f190356
eddie.lys <ysh.li@misumi.sh.cn>
2023-09-14 13:41:10 +0800

    update: Add heterogeneous project

---
+++ micro-service-dubbo/micro-service-dubbo-data/main.py
@@ -0,0 +1,13 @@
+print("first")
+
+
+def say_hello():
+    str = "hello"
+    print(str);
+    print(__name__ + 'from hello.sayhello()')
+
+
+if __name__ == "__main__":
+    print('This is main of module "hello.py"')
+    say_hello()
+    print(__name__ + 'from hello.main')
---
+++ micro-service-dubbo/micro-service-dubbo-message/go.mod
@@ -0,0 +1,3 @@
+module micro-service-dubbo-message
+
+go 1.21.1
