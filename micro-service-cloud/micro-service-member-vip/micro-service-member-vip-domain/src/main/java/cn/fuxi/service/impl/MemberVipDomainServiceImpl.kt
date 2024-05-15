c199f46ef41c48044bba689b17dd8101b7da3508
eddie.lys <ysh.li@misumi.sh.cn>
2024-05-15 16:43:52 +0800

    update: add member-api modul

--- micro-service-cloud/micro-service-member-vip/micro-service-member-vip-domain/src/main/java/cn/fuxi/service/impl/MemberVipDomainServiceImpl.kt
+++ micro-service-cloud/micro-service-member-vip/micro-service-member-vip-domain/src/main/java/cn/fuxi/service/impl/MemberVipDomainServiceImpl.kt
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service
 @Service
 class MemberVipDomainServiceImpl : MemberVipDomainService {
 
-    override fun defineUserAsVIP() {
+    override fun defineUserAsVIP(): Boolean {
         TODO("Not yet implemented")
     }
 }
