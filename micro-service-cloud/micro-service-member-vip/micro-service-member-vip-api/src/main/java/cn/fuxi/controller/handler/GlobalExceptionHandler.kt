a5b3add14ad5a3506ee399dcf9ac325d136d1905
eddie.lys <ysh.li@misumi.sh.cn>
2024-05-15 09:45:23 +0800

    update: add member-api modul

--- micro-service-cloud/micro-service-member-vip/micro-service-member-vip-api/src/main/java/cn/fuxi/controller/handler/GlobalExceptionHandler.kt
+++ micro-service-cloud/micro-service-member-vip/micro-service-member-vip-api/src/main/java/cn/fuxi/controller/handler/GlobalExceptionHandler.kt
@@ -6,6 +6,7 @@ import com.olympus.base.utils.support.globalization.GlobalMessage
 import com.olympus.logger.logger.SmileEventLogger
 import com.olympus.smile.config.Environment
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.context.support.DefaultMessageSourceResolvable
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -21,35 +22,37 @@ import java.util.stream.Collectors
  */
 @ResponseBody
 @ControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler
 
-    @ExceptionHandler(Throwable::class)
-    fun exceptionHandler(request: HttpServletRequest?, e: Exception): Response<String> {
-        SmileEventLogger.error("全局异常处理器拦截异常: {}", ExceptionHelper.getExceptionDetail(e))
-        if (Environment.getInstance().isDaily || Environment.getInstance().isStaging) {
-            return Response.ofError(e.message)
+@ExceptionHandler(Throwable::class)
+fun exceptionHandler(request: HttpServletRequest?, e: Exception): Response<String> {
+    SmileEventLogger.error("全局异常处理器拦截异常: {}", ExceptionHelper.getExceptionDetail(e))
+    if (Environment.getInstance().isDaily || Environment.getInstance().isStaging) {
+        return Response.ofError(e.message)
+    }
+    return Response.ofError(GlobalMessage.of("P-BASICS-ERROR-GLOBAL-0001"))
+}
+
+@ExceptionHandler(MethodArgumentNotValidException::class)
+fun exceptionHandler(e: MethodArgumentNotValidException): Response<String> {
+    SmileEventLogger.error("入参缺失拦截异常: {}", ExceptionHelper.getExceptionDetail(e))
+    val message: String = e.bindingResult
+        .allErrors
+        .stream()
+        .map {
+            defaultMessageSourceResolvable: DefaultMessageSourceResolvable ->
+            GlobalMessage.of(defaultMessageSourceResolvable.defaultMessage)
         }
-        return Response.ofError(GlobalMessage.of("P-MATERIAL-00245"))
-    }
+        .collect(Collectors.joining(","))
+    return Response.ofError(message)
+}
 
-    @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun exceptionHandler(e: MethodArgumentNotValidException): Response<String> {
-        SmileEventLogger.error("入参缺失拦截异常: {}", ExceptionHelper.getExceptionDetail(e))
-        val message: String = e.bindingResult
-            .allErrors
-            .stream()
-            .map { defaultMessageSourceResolvable -> GlobalMessage.of(defaultMessageSourceResolvable.getDefaultMessage()) }
-            .collect(Collectors.joining(","))
-        return Response.ofError(message)
-    }
-
-    /**
-     * 针对参数解析的异常单独处理
-     * 便于前端发现和定位问题
-     */
-    @ExceptionHandler(HttpMessageNotReadableException::class)
-    fun exceptionHandler(e: HttpMessageNotReadableException): Response<String> {
-        SmileEventLogger.error("http请求参数转换异常: {}", ExceptionHelper.getExceptionDetail(e))
-        return Response.ofError(GlobalMessage.of("P-MATERIAL-00245"), e.message)
-    }
+/**
+ * 针对参数解析的异常单独处理
+ * 便于前端发现和定位问题
+ */
+@ExceptionHandler(HttpMessageNotReadableException::class)
+fun exceptionHandler(e: HttpMessageNotReadableException): Response<String> {
+    SmileEventLogger.error("http请求参数转换异常: {}", ExceptionHelper.getExceptionDetail(e))
+    return Response.ofError(GlobalMessage.of("P-BASICS-ERROR-GLOBAL-CONVERT-0001"), e.message)
 }
---
+++ micro-service-cloud/micro-service-member-vip/micro-service-member-vip-domain/src/main/java/cn/fuxi/service/MemberVipDomainService.kt
@@ -0,0 +1,14 @@
+package cn.fuxi.service
+
+/**
+ * 会员主服务
+ * @since 2024/5/14
+ * @author eddie.lys
+ */
+interface MemberVipDomainService {
+
+    /**
+     * 定义一个用户为VIP
+     */
+    fun defineUserAsVIP()
+}
